15ce5fdb6022cd5ffc105736ec49a0f88b1bb9db
RG <goyalrohan2020@gmail.com>
2024-05-04 23:30:19 +0530

    feat: table from env

--- WebGLTable.py
+++ WebGLTable.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
@@ -5,7 +7,7 @@ from datetime import datetime
 Base = declarative_base()
 
 class WebGLTable(Base):
-    __tablename__ = "webgl_grt"
+    __tablename__ = os.environ.get("DB_TABLE")
     User_id = Column(String, primary_key=True)
     Timestamp = Column(DateTime, primary_key=True, default=datetime.utcnow)
     Event = Column(String)
